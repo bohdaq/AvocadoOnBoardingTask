37a2e1b43f5b225d2c5fb287708334c2520414b3
Bogdan Tsap <bogdan.tsap@thinktank.net>
2016-04-18 13:12:03 +0300

    checkboxes on first screen

---
+++ app/elements/my-datastore/my-datastore.js
@@ -0,0 +1,29 @@
+(function() {
+  'use strict';
+
+  Polymer({
+    is: 'my-datastore',
+
+    properties: {
+      configuration: {
+        notify: true, //2way data binding
+        value: function () {
+          return [
+            {
+              name: 'buy milk',
+              selected: true
+            },
+            {
+              name: 'learn-polymer',
+              selected: true
+            },
+            {
+              name: 'clean house',
+              selected: false
+            }
+          ]
+        }
+      }
+    }
+  });
+})();
