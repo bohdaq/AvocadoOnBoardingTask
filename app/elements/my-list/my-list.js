35cf007cac9bd432a7c52ffb70e78f1112b03fb2
Bogdan Tsap <bogdan.tsap@thinktank.net>
2016-04-18 15:59:50 +0300

    filtering

--- app/elements/my-list/my-list.js
+++ app/elements/my-list/my-list.js
@@ -4,8 +4,12 @@
   Polymer({
     is: 'my-list',
 
-    properties: {},
-
-    ready: function(){}
+    _filter: function(val) {
+      return function(item) {
+        if (!val) return true;
+        if (!item) return false;
+        return (item.name && ~item.name.indexOf(val));
+      };
+    }
   });
 })();
