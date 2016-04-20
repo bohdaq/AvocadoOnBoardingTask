72a98b2042463d882d885fa703f076c3e8074705
Bogdan Tsap <bogdan.tsap@thinktank.net>
2016-04-20 08:39:10 +0300

    dropdown added

--- app/elements/display-item/display-item.js
+++ app/elements/display-item/display-item.js
@@ -2,6 +2,26 @@
   'use strict';
 
   Polymer({
-    is: 'display-item'
+    is: 'display-item',
+
+    properties: {
+      // forces the dom reflect the dom-if and not hiding.
+      // slows a bit perfomance but logically is more correct
+      restamp: true
+    },
+
+    _isTextOrNumberType: function (type) {
+      console.log(type);
+      if (type === 'text' || type === 'number')
+        return true;
+      return false;
+    },
+
+    _isDropdown: function (type) {
+      console.log(type);
+      if (type === 'dropdown')
+        return true;
+      return false;
+    }
   });
 })();
