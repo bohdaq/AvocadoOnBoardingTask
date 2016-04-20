2f96f9550e7586a5e1028b5651424ad464b50017
Bogdan Tsap <bogdan.tsap@thinktank.net>
2016-04-20 08:55:47 +0300

    datepicker added

--- app/elements/display-item/display-item.js
+++ app/elements/display-item/display-item.js
@@ -5,21 +5,25 @@
     is: 'display-item',
 
     properties: {
-      // forces the dom reflect the dom-if and not hiding.
+      // forces the dom reflect the dom-if and not hiding instead.
       // slows a bit perfomance but logically is more correct
       restamp: true
     },
 
     _isTextOrNumberType: function (type) {
-      console.log(type);
       if (type === 'text' || type === 'number')
         return true;
       return false;
     },
 
     _isDropdown: function (type) {
-      console.log(type);
       if (type === 'dropdown')
+        return true;
+      return false;
+    },
+
+    _isDatepicker: function (type) {
+      if (type === 'datepicker')
         return true;
       return false;
     }
